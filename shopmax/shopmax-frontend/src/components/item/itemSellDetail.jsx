74883da7635292af82859914067cf434b39b01fd
hado0123 <hado_123@naver.com>
2025-01-10 15:14:54 +0900

    up

--- shopmax/shopmax-frontend/src/components/item/itemSellDetail.jsx
+++ shopmax/shopmax-frontend/src/components/item/itemSellDetail.jsx
@@ -2,6 +2,8 @@ import { Box, Typography, Button, Alert, CardMedia } from '@mui/material'
 import Grid from '@mui/material/Grid2'
 import { NumberInput } from '../../styles/NumberInputBasic'
 import LocalMallIcon from '@mui/icons-material/LocalMall'
+import { fetchItemByIdThunk } from '../../features/itemSlice'
+import { formatWithComma } from '../../utils/priceSet'
 
 import React, { useState, useEffect, useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
@@ -10,6 +12,12 @@ import { useParams } from 'react-router-dom'
 function ItemSellDetail() {
    const { id } = useParams() // item의 id
    const dispatch = useDispatch()
+   const { item, error, loading } = useSelector((state) => state.items)
+
+   //상품 데이터 불러오기
+   useEffect(() => {
+      dispatch(fetchItemByIdThunk(id))
+   }, [dispatch, id])
 
    if (loading) {
       return null //아무것도 보여주지 X
@@ -36,29 +44,29 @@ function ItemSellDetail() {
                   <Grid container spacing={10}>
                      {/* 왼쪽 이미지 */}
                      <Grid xs={12} md={6}>
-                        <CardMedia component="img" image={} alt={} sx={{ width: '450px', borderRadius: '8px' }} />
+                        <CardMedia component="img" image={`${process.env.REACT_APP_API_URL}${item.Imgs.filter((img) => img.repImgYn === 'Y')[0].imgUrl}`} alt={item.itemNm} sx={{ width: '450px', borderRadius: '8px' }} />
                      </Grid>
 
                      {/* 오른쪽 상세 정보 */}
                      <Grid xs={12} md={6}>
                         <Typography variant="h4" gutterBottom>
                            <LocalMallIcon sx={{ color: '#ffab40', fontSize: '32px' }} />
-                           {}
+                           {item.itemNm}
                         </Typography>
 
                         <Typography variant="h6" gutterBottom>
-                           가격: {}원
+                           가격: {formatWithComma(String(item.price))}원
                         </Typography>
                         <Typography variant="body2" color="text.secondary" gutterBottom>
-                           현재 재고: {}개
+                           현재 재고: {item.stockNumber}개
                         </Typography>
 
                         {item.itemSellStatus === 'SOLD_OUT' ? (
                            <Alert severity="error">품절</Alert>
                         ) : (
                            <Box sx={{ display: 'flex', flexDirection: 'column', gap: '16px', width: '300px' }}>
-                              <NumberInput aria-label="Demo number input" placeholder="수량" value={} onChange={} min={1} max={item.stockNumber} />
-                              <Typography variant="h6">총 가격: {}원</Typography>
+                              <NumberInput aria-label="Demo number input" placeholder="수량" min={1} max={item.stockNumber} />
+                              <Typography variant="h6">총 가격: 원</Typography>
                               <Button variant="contained" color="primary">
                                  구매하기
                               </Button>
@@ -74,12 +82,12 @@ function ItemSellDetail() {
                      상세 정보
                   </Typography>
                   <Typography variant="body1" gutterBottom sx={{ mt: 4, mb: 5 }}>
-                     {}
+                     {item.itemDetail}
                   </Typography>
                   <Grid container spacing={2}>
                      {item.Imgs.map((img, index) => (
                         <Grid xs={12} sm={6} md={4} key={index}>
-                           <CardMedia component="img" image={} alt={`상세 이미지 ${index + 1}`} sx={{ width: '100%', borderRadius: '8px' }} />
+                           <CardMedia component="img" image={`${process.env.REACT_APP_API_URL}${img.imgUrl}`} alt={`상세 이미지 ${index + 1}`} sx={{ width: '100%', borderRadius: '8px' }} />
                         </Grid>
                      ))}
                   </Grid>
